4bd63d8bcd7fd0db78abf523d99ebd11ddb93a7a
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-09-27 17:53:48 +0900

    [level 0] Title: 편지, Time: 0.61 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(String message) {
-       return message.length() * 2;
+       return (int) Arrays.stream(message.split("")).count() * 2;
     }
 }
