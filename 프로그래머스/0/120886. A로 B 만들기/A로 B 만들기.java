d63e8fe8d8cdbe916c7eed4d7d184c005ac08a24
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-01 01:37:26 +0900

    [level 0] Title: A로 B 만들기, Time: 1.09 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -2,25 +2,15 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(String before, String after) {
-        
-        int[] beforeArray = Arrays.stream(getASCII(before)).sorted().toArray();
-        int[] afterArray = Arrays.stream(getASCII(after)).sorted().toArray();
-        
-       if (Arrays.equals(beforeArray, afterArray)) {
+        char[] beforeCharArray = before.toCharArray();
+        char[] afterCharArray = after.toCharArray();
+
+        Arrays.sort(beforeCharArray);
+        Arrays.sort(afterCharArray);
+
+        if (Arrays.equals(beforeCharArray, afterCharArray)) {
             return 1;
-        } else {
-            return 0;
         }
-    }
-    
-    public int[] getASCII(String string) {
-        int[] codes = new int[string.length()];
-
-        for (int i = 0; i < string.length(); i++) {
-            int code = string.charAt(i);
-            codes[i] = code;
-        }
-
-        return codes;
+        return 0;
     }
 }
