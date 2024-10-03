11386f58a57cc967596975367c8e08568cde61d7
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-04 00:31:34 +0900

    [level 0] Title: n의 배수 고르기, Time: 4.82 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        return Arrays.stream(numlist)
+                .filter(num -> num % n == 0)
+                .toArray();
+    }
+}
