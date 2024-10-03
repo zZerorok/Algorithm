0c97db4871ba794fb6d7ee72640efa6b2c943cd8
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-04 00:01:17 +0900

    [level 0] Title: 모음 제거, Time: 0.32 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,5 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        return my_string.replaceAll("[aeiou]", "");
+        return my_string.replaceAll("a", "").replaceAll("e", "").replaceAll("i", "").replaceAll("o", "").replaceAll("u", "");
     }
 }
