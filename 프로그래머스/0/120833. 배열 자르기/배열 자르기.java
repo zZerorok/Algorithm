140fed753ef6ed2250bcba9c863dc47ab69b1134
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-03 23:43:09 +0900

    [level 0] Title: 배열 자르기, Time: 6.04 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        return IntStream.rangeClosed(num1, num2).map(it -> numbers[it]).toArray();
+    }
+}
