d5a83d517510760297173979186d87137d5d5d18
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-06 23:57:39 +0900

    [level 0] Title: 카운트 업, Time: 3.01 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,12 +1,6 @@
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[end_num - start_num + 1];
-        int index = 0;
-        
-        for (int i = start_num; i <= end_num; i++) {
-            answer[index++] = i;
-        }
-        
-        return answer;
+        return IntStream.rangeClosed(start_num, end_num).toArray();
     }
 }
