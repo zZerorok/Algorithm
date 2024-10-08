bd1c03ddfc8373c07321b007dc6c74cd49642408
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-08 22:12:18 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,9 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length + 1];
-        for (int i = 0; i < num_list.length; i++) {
-            answer[i] = num_list[i];
-        }
+        int[] answer = Arrays.copyOf(num_list, num_list.length + 1);
         
         int lastIndexNum = num_list[num_list.length - 1];
         int beforeLastIndexNum = num_list[num_list.length - 2];
