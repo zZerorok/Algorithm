2275db782713ad2ef517cb8d7bdbe02304d9ca4e
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-17 00:45:25 +0900

    [level 0] Title: 배열 비교하기, Time: 1.17 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if (arr1.length > arr2.length) {
+            return 1;
+        } else if (arr1.length < arr2.length) {
+            return -1;
+        }
+        
+        int arr1Sum = Arrays.stream(arr1).sum();
+        int arr2Sum = Arrays.stream(arr2).sum();
+
+        if (arr1Sum < arr2Sum) {
+            return -1;
+        } else if (arr1Sum > arr2Sum) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
