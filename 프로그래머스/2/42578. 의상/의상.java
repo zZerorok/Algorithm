7fef261ce9f3296df7d0930e052b9984ed85fe64
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-10-26 12:47:54 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -9,12 +9,6 @@ class Solution {
             map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
 
-        if (map.size() == 1) {
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                return entry.getValue();
-            }
-        }
-
         int combinationCount = 1;
         for (Integer value : map.values()) {
             combinationCount *= (value + 1);
