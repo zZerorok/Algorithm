b4971ca2619d8ea3ec06228136fe70c49b8d358c
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-11-01 13:34:04 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 10.31 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
+
+        if (myString.contains(pat)) {
+            return 1;
+        }
+        return 0;
+    }
+}
