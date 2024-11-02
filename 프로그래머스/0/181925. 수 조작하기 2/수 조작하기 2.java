13ff6336e7232980913d79f4af372e4d403e6a3e
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-11-02 19:14:17 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.33 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,21 +1,18 @@
 class Solution {
     public String solution(int[] numLog) {
         StringBuilder answer = new StringBuilder();
-        
+
         for (int i = 1; i < numLog.length; i++) {
-            int result = 0;
-            result = numLog[i] - numLog[i - 1];
-            if (result == 1) {
-                answer.append("w");
-            } else if (result == -1) {
-                answer.append("s");
-            } else if (result == 10) {
-                answer.append("d");
-            } else if (result == -10) {
-                answer.append("a");
+            int result = numLog[i] - numLog[i - 1];
+
+            switch (result) {
+                case 1 -> answer.append("w");
+                case -1 -> answer.append("s");
+                case 10 -> answer.append("d");
+                case -10 -> answer.append("a");
             }
         }
-
+        
         return answer.toString();
     }
 }
