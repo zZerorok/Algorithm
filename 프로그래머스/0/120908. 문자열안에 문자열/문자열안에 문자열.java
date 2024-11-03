6c9a6766f1bba33904ab49807032ef67efa4342c
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-11-03 19:00:58 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        if (str1.contains(str2)) {
-            return 1;
-        }
-        return 2;
+        return str1.contains(str2) ? 1 : 2;
     }
 }
