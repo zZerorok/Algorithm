6f3ffaa26f83cf1d76598e246fa9efea34e8f6d9
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-11-15 19:08:42 +0900

    [level 0] Title: 배열 만들기 1, Time: 11.78 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -5,8 +5,7 @@ import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n, int k) {
        return IntStream.rangeClosed(1, n)
-                .filter(num -> (num * k) <= n)
-                .map(num -> num * k)
+                .filter(num -> num % k == 0)
                 .toArray();
     }
 }
