1d668106646c636464c5f81dc675279f116dc425
youngrok <156323937+zZerorok@users.noreply.github.com>
2024-12-10 20:44:59 +0900

    [level 3] Title: 단어 변환, Time: 0.68 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,20 +1,20 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
- if (!Set.of(words).contains(target)) {
+        if (!Set.of(words).contains(target)) {
             return 0;
         }
 
-        boolean[] visited = new boolean[words.length];
+        Map<String, Boolean> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, false);
+        }
 
         Queue<String> queue = new LinkedList<>();
         queue.add(begin);
 
         int step = 0;
-
         while (!queue.isEmpty()) {
             for (int i = 0; i < queue.size(); i++) {
                 String currentWord = queue.poll();
@@ -23,18 +23,21 @@ class Solution {
                     return step;
                 }
 
-                checkWord(words, visited, queue, currentWord);
+                processWords(map, queue, currentWord);
             }
             step++;
         }
         return 0;
     }
 
-    private void checkWord(String[] words, boolean[] visited, Queue<String> queue, String currentWord) {
-        for (int i = 0; i < words.length; i++) {
-            if (!visited[i] && isConvertible(currentWord, words[i])) {
-                visited[i] = true;
-                queue.add(words[i]);
+    private void processWords(Map<String, Boolean> map, Queue<String> queue, String currentWord) {
+        for (Map.Entry<String, Boolean> entry : map.entrySet()) {
+            String word = entry.getKey();
+            Boolean isVisited = entry.getValue();
+
+            if (!isVisited && isConvertible(currentWord, word)) {
+                map.put(word, true);
+                queue.add(word);
             }
         }
     }
