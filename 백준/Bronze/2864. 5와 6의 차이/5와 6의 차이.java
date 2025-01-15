5f0149e1e75876620d2fab2406466c2870a99208
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-01-15 17:16:26 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 128 ms, Memory: 16148 KB -BaekjoonHub

--- 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.java
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String first = scanner.next();
-        String second = scanner.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = reader.readLine().split(" ");
 
-        int firstMin = Integer.parseInt(first.replace('6', '5'));
-        int secondMin = Integer.parseInt(second.replace('6', '5'));
-        int minSum = firstMin + secondMin;
-
-        int firstMax = Integer.parseInt(first.replace('5', '6'));
-        int secondMax = Integer.parseInt(second.replace('5', '6'));
-        int maxSum = firstMax + secondMax;
+        int minSum = calculateSum(input, '6', '5');
+        int maxSum = calculateSum(input, '5', '6');
 
         System.out.println(minSum + " " + maxSum);
+    }
+    
+    private static int calculateSum(String[] input, char from, char to) {
+        return Arrays.stream(input)
+                .map(num -> Integer.parseInt(num.replace(from, to)))
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
