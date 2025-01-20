64be138b77f0291b80c2d68b32c13bc559af279b
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-01-21 00:38:29 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1 = scanner.nextInt();
+        double num2 = scanner.nextInt();
+
+        System.out.println(num1 / num2);
+    }
+}
