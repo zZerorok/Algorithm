6224e2be0eaa2c24d724cb4317cb45cf39ea98bc
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-02-05 16:20:14 +0900

    [Silver V] Title: 소트인사이드, Time: 164 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -4,20 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String[] split = input.split("");
-        List<Integer> numbers = new ArrayList<>();
-        for (String num : split) {
-            numbers.add(Integer.parseInt(num));
-        }
-        
-        numbers.sort(Collections.reverseOrder());
-        
+        char[] digits = scanner.nextLine().toCharArray();
+
+        Arrays.sort(digits);
+
         StringBuilder builder = new StringBuilder();
-        for (Integer number : numbers) {
-            builder.append(number);
+        for (char digit : digits) {
+            builder.append(digit);
         }
-        
-        System.out.println(builder);
+
+        System.out.println(builder.reverse());
     }
 }
