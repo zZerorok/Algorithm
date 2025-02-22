d5c64ab0501118a713c5d547252e55fee478e395
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-02-22 18:23:27 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 920 ms, Memory: 90228 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -3,60 +3,96 @@ import java.util.*;
 
 public class Main {
 
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final Map<Integer, Node> NODES = new HashMap<>();
+    private static final List<Vertex> VERTICES = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int nodeCount = Integer.parseInt(READER.readLine());
+        readConnections(nodeCount - 1);
+        processNode(nodeCount);
+        writeParentNode();
+    }
 
-        Map<Integer, Node> nodes = new HashMap<>();
-        int nodeCount = Integer.parseInt(reader.readLine());
-        for (int i = 1; i <= nodeCount; i++) {
-            nodes.put(i, new Node(i));
-        }
-
-        for (int i = 0; i < nodeCount - 1; i++) {
-            String[] split = reader.readLine().split(" ");
-            int num1 = Integer.parseInt(split[0]);
-            int num2 = Integer.parseInt(split[1]);
-
-            Node node1 = nodes.get(num1);
-            Node node2 = nodes.get(num2);
-            node1.addChild(node2);
-            node2.addChild(node1);
-        }
-
-        dfs(nodes.get(1), null);
-
-        for (int i = 2; i <= nodeCount; i++) {
-            System.out.println(nodes.get(i).parent.number);
+    private static void readConnections(int vertexCount) throws IOException {
+        for (int i = 0; i < vertexCount; i++) {
+            String[] split = READER.readLine().split(" ");
+            VERTICES.add(new Vertex(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
         }
     }
 
-    private static void dfs(Node node, Node parent) {
-        node.setParent(parent);
+    private static void processNode(int nodeCount) {
+        initNode(nodeCount);
+        linkNode();
+        Node rootNode = NODES.get(1);
+        setParentNode(rootNode, null);
+    }
 
-        for (Node child : node.children) {
-            if (child.parent == null) {
-                dfs(child, node);
+    private static void initNode(int count) {
+        for (int i = 1; i <= count; i++) {
+            NODES.put(i, new Node(i));
+        }
+    }
+
+    private static void linkNode() {
+        for (Vertex vertex : VERTICES) {
+            Node node1 = NODES.get(vertex.start);
+            Node node2 = NODES.get(vertex.end);
+            node1.link(node2);
+            node2.link(node1);
+        }
+    }
+
+    private static void setParentNode(Node currentNode, Node parentNode) {
+        currentNode.setParent(parentNode);
+
+        for (Node nextNode : currentNode.nextNodes) {
+            if (nextNode.parent == null) {
+                setParentNode(nextNode, currentNode);
             }
         }
     }
 
+    private static void writeParentNode() throws IOException {
+        for (int i = 2; i <= NODES.size(); i++) {
+            WRITER.write(NODES.get(i).parentNumber() + System.lineSeparator());
+        }
+        WRITER.flush();
+        WRITER.close();
+    }
+
+    static class Vertex {
+        private final int start;
+        private final int end;
+
+        public Vertex(int first, int second) {
+            this.start = first;
+            this.end = second;
+        }
+    }
+
     static class Node {
-        private final int number;
         private Node parent;
-        private final List<Node> children;
+        private final int number;
+        private final List<Node> nextNodes;
 
-        public Node(int number) {
-            this.number = number;
+        private Node(int number) {
             this.parent = null;
-            this.children = new ArrayList<>();
+            this.number = number;
+            this.nextNodes = new ArrayList<>();
         }
 
-        public void addChild(Node child) {
-            this.children.add(child);
+        public int parentNumber() {
+            return parent != null ? parent.number : 0;
         }
 
-        public void setParent(Node parent) {
-            this.parent = parent;
+        public void link(Node targetNode) {
+            nextNodes.add(targetNode);
+        }
+
+        public void setParent(Node parentNode) {
+            this.parent = parentNode;
         }
     }
 }
