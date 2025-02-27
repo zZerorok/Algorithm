a540743cd5933638f37cf9aeb9ef71840cef283f
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-02-28 02:50:22 +0900

    [Bronze V] Title: 합, Time: 176 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-import java.util.stream.IntStream;
+import java.util.*;
 
 public class Main {
+    
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int n = scanner.nextInt();
-
-        System.out.println(IntStream.rangeClosed(1, n).sum());
+        System.out.println(n * (n + 1) / 2);
     }
 }
