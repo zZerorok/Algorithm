607c73895d66e6e8e6fa4007bb3020cea8660fc4
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-05 21:42:06 +0900

    [Bronze II] Title: 막대기, Time: 260 ms, Memory: 25996 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.java
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -1,16 +1,15 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 import java.util.Stack;
 
 public class Main {
 
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
     private static final Stack<Integer> TOWERS = new Stack<>();
 
     public static void main(String[] args) throws IOException {
-        int N = SCANNER.nextInt();
+        int N = Integer.parseInt(READER.readLine());
         for (int i = 0; i < N; i++) {
-            TOWERS.push(SCANNER.nextInt());
+            TOWERS.push(Integer.parseInt(READER.readLine()));
         }
 
         int count = 0;
