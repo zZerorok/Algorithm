8e2c5e42dec9140fb9ab44aa8bc7692f581aa38f
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-10 23:07:14 +0900

    [Bronze II] Title: 대표값2, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -3,21 +3,26 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int count = 5;
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final List<Integer> numbers = new ArrayList<>();
+    private static final int COUNT = 5;
 
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i <= count; i++) {
-            numbers.add(Integer.parseInt(reader.readLine()));
+    public static void main(String[] args) throws IOException {
+        int sum = 0;
+        for (int i = 1; i <= COUNT; i++) {
+            int number = Integer.parseInt(READER.readLine());
+            numbers.add(number);
+            sum += number;
         }
+
         Collections.sort(numbers);
 
-        int average = numbers.stream().mapToInt(Integer::intValue).sum() / count;
-
-        Integer medianNumber = numbers.get((count - 1) / 2);
-
-        System.out.println(average);
-        System.out.println(medianNumber);
+        WRITER.write(String.valueOf(sum / COUNT));
+        WRITER.newLine();
+        WRITER.write(String.valueOf(numbers.get((COUNT - 1) / 2)));
+        WRITER.flush();
+        WRITER.close();
+        READER.close();
     }
 }
