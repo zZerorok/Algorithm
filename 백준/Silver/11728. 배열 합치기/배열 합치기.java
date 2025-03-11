65bac76e9ee0dd1fde73171007fd545b8d554b0a
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-11 22:39:56 +0900

    [Silver V] Title: 배열 합치기, Time: 1540 ms, Memory: 349812 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -8,12 +8,12 @@ public class Main {
     private static final List<Integer> VALUES = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        String[] arraySizes = READER.readLine().split(" ");
-        String firstInput = READER.readLine();
-        String secondInput = READER.readLine();
+        StringTokenizer tokenizer = new StringTokenizer(READER.readLine());
+        int firstSize = Integer.parseInt(tokenizer.nextToken());
+        int secondSize = Integer.parseInt(tokenizer.nextToken());
 
-        addElements(firstInput, Integer.parseInt(arraySizes[0]));
-        addElements(secondInput, Integer.parseInt(arraySizes[1]));
+        addElements(READER.readLine(), firstSize);
+        addElements(READER.readLine(), secondSize);
         sort();
         writeValues();
 
@@ -23,14 +23,14 @@ public class Main {
     }
 
     private static void addElements(String input, int size) {
-        String[] array = input.split(" ");
+        StringTokenizer tokenizer = new StringTokenizer(input);
         for (int i = 0; i < size; i++) {
-            VALUES.add(Integer.parseInt(array[i]));
+            VALUES.add(Integer.parseInt(tokenizer.nextToken()));
         }
     }
 
     private static void sort() {
-        Collections.sort(VALUES);
+        VALUES.sort(null);
     }
 
     private static void writeValues() throws IOException {
@@ -38,6 +38,6 @@ public class Main {
         for (Integer value : VALUES) {
             builder.append(value).append(" ");
         }
-        WRITER.write(builder.toString().trim());
+        WRITER.write(builder.toString());
     }
 }
