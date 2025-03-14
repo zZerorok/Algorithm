e7e94b8d0ef2c40d24c98663efbc3b6eabbb8141
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-14 21:48:37 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 148 ms, Memory: 16144 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -1,30 +1,38 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int C = Integer.parseInt(reader.readLine());
+        int C = Integer.parseInt(READER.readLine());
 
         for (int i = 0; i < C; i++) {
-            String[] input = reader.readLine().split(" ");
+            String[] input = READER.readLine().split(" ");
             int studentCount = Integer.parseInt(input[0]);
-            int sum = 0;
+
+            List<Integer> scores = new ArrayList<>();
             for (int j = 1; j <= studentCount; j++) {
                 int score = Integer.parseInt(input[j]);
-                sum += score;
+                scores.add(score);
             }
-            double average = (double) sum / studentCount;
+            
+            double average = (double) scores.stream().mapToInt(Integer::intValue).sum() / studentCount;
 
             int aboveAverageCount = 0;
-            for (int j = 1; j <= studentCount ; j++) {
-                if (average < Integer.parseInt(input[j])) {
-                    aboveAverageCount++;
+            for (Integer score : scores) {
+                if (score > average) {
+                    aboveAverageCount += 1;
                 }
             }
 
             double result = (double) aboveAverageCount / studentCount * 100;
-            System.out.println(String.format("%.3f", result) + "%");
+            WRITER.write(String.format("%.3f", result) + "%");
+            WRITER.newLine();
         }
+
+        WRITER.flush();
     }
 }
