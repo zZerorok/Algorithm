13f83c50623988ea4f1e8dce430dd6bc56c6820d
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-14 22:03:30 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 144 ms, Memory: 15808 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 
@@ -7,32 +6,44 @@ public class Main {
     private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        int C = Integer.parseInt(READER.readLine());
-
-        for (int i = 0; i < C; i++) {
+        int testCase = Integer.parseInt(READER.readLine());
+        for (int i = 0; i < testCase; i++) {
             String[] input = READER.readLine().split(" ");
             int studentCount = Integer.parseInt(input[0]);
-
-            List<Integer> scores = new ArrayList<>();
-            for (int j = 1; j <= studentCount; j++) {
-                int score = Integer.parseInt(input[j]);
-                scores.add(score);
-            }
+            int[] scores = new int[studentCount];
             
-            double average = (double) scores.stream().mapToInt(Integer::intValue).sum() / studentCount;
+            int sum = sum(input, scores);
 
-            int aboveAverageCount = 0;
-            for (Integer score : scores) {
-                if (score > average) {
-                    aboveAverageCount += 1;
-                }
-            }
+            double average = (double) sum / studentCount;
+            
+            int aboveAverageCount = countAboveAverage(scores, average);
 
             double result = (double) aboveAverageCount / studentCount * 100;
+
             WRITER.write(String.format("%.3f", result) + "%");
             WRITER.newLine();
         }
 
         WRITER.flush();
+    }
+
+    private static int sum(String[] input, int[] scores) {
+        int sum = 0;
+        for (int j = 0; j < scores.length; j++) {
+            int score = Integer.parseInt(input[j + 1]);
+            scores[j] = score;
+            sum += score;
+        }
+        return sum;
+    }
+
+    private static int countAboveAverage(int[] scores, double average) {
+        int aboveAverageCount = 0;
+        for (int score : scores) {
+            if (score > average) {
+                aboveAverageCount += 1;
+            }
+        }
+        return aboveAverageCount;
     }
 }
