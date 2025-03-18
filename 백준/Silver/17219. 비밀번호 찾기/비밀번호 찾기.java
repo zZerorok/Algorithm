6ad558d9b62d1282cc07e5145fd7f86566f19615
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-18 23:14:13 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 640 ms, Memory: 62732 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final Map<String, String> SITE_PASSWORDS = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        String[] counts = READER.readLine().split(" ");
+        int siteCount = Integer.parseInt(counts[0]);
+        int searchPasswordCount = Integer.parseInt(counts[1]);
+
+        for (int i = 0; i < siteCount; i++) {
+            String[] sitePasswordPair = READER.readLine().split(" ");
+            SITE_PASSWORDS.put(sitePasswordPair[0], sitePasswordPair[1]);
+        }
+
+        List<String> sites = new ArrayList<>();
+        for (int i = 0; i < searchPasswordCount; i++) {
+            String site = READER.readLine();
+            sites.add(site);
+        }
+
+        for (String site : sites) {
+            if (SITE_PASSWORDS.containsKey(site)) {
+                String password = SITE_PASSWORDS.get(site);
+                WRITER.write(password);
+                WRITER.newLine();
+            }
+        }
+
+        WRITER.flush();
+        WRITER.close();
+        READER.close();
+    }
+}
