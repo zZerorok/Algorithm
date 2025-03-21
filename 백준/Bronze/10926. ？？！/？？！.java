bd17722bcfe39baff28fe6a2eda3375781c6cc33
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-22 01:01:45 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        System.out.println(line + "??!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        writer.write(reader.readLine() + "??!");
+        writer.flush();
     }
 }
