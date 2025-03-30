8ab3697a111235fea00454d8edd444b630dee91e
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-03-30 12:40:35 +0900

    [Silver IV] Title: 카드, Time: 300 ms, Memory: 30004 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -4,40 +4,41 @@ import java.util.*;
 public class Main {
 
     private static final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter WRITER = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
         int cardCount = Integer.parseInt(READER.readLine());
         Map<Long, Integer> frequencyMap = initCardMap(cardCount);
-        int maxFrequency = getMaxFrequency(frequencyMap);
-        long result = findMinKeyWithMaxFrequency(frequencyMap, maxFrequency);
-        printResult(result);
+        long result = findMinKeyWithMaxFrequency(frequencyMap);
+        System.out.println(result);
     }
 
     private static Map<Long, Integer> initCardMap(int cardCount) throws IOException {
         Map<Long, Integer> map = new TreeMap<>();
+        
         for (int i = 0; i < cardCount; i++) {
             long number = Long.parseLong(READER.readLine());
             map.merge(number, 1, Integer::sum);
         }
+        
         return map;
     }
 
-    private static int getMaxFrequency(Map<Long, Integer> map) {
-        return Collections.max(map.values());
-    }
-
-    private static long findMinKeyWithMaxFrequency(Map<Long, Integer> map, int maxFrequency) {
+    private static long findMinKeyWithMaxFrequency(Map<Long, Integer> map) {
+        int maxFrequency = 0;
+        long resultKey = Long.MAX_VALUE;
+        
         for (Map.Entry<Long, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == maxFrequency) {
-                return entry.getKey();
+            long key = entry.getKey();
+            int frequency = entry.getValue();
+            
+            if (frequency > maxFrequency) {
+                maxFrequency = frequency;
+                resultKey = key;
+            } else if (frequency == maxFrequency && key < resultKey) {
+                resultKey = key;
             }
         }
-        return -1;
-    }
-
-    private static void printResult(long result) throws IOException {
-        WRITER.write(String.valueOf(result));
-        WRITER.flush();
+        
+        return resultKey;
     }
 }
