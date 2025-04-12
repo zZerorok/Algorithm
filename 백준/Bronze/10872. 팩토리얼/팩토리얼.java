5c16c50e90195bc74b1324023948a3ebf7b659ee
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-04-13 01:48:08 +0900

    [Bronze III] Title: 팩토리얼, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,16 +1,23 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int result = 1;
+        int n = Integer.parseInt(reader.readLine());
+        long result = calculateFactorial(n);
+
+        writer.write(String.valueOf(result));
+        writer.flush();
+    }
+
+    private static long calculateFactorial(int n) {
+        long result = 1;
         for (int i = 1; i <= n; i++) {
             result *= i;
         }
-
-        System.out.println(result);
+        return result;
     }
 }
