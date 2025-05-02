1539b9e1501d40ceb59383817875913fbce3fc55
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-05-02 21:49:34 +0900

    [Silver IV] Title: 야근, Time: 424 ms, Memory: 46956 KB -BaekjoonHub

---
+++ 백준/Silver/14402. 야근/야근.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        Map<String, Integer> employees = new HashMap<>();
+
+        int entryCount = Integer.parseInt(reader.readLine());
+        int overtimeCount = 0;
+        for (int i = 0; i < entryCount; i++) {
+            String[] entryInfo = reader.readLine().split(" ");
+            String employeeName = entryInfo[0];
+            String entryStatus = entryInfo[1];
+
+            if (entryStatus.equals("+")) {
+                employees.put(employeeName, employees.getOrDefault(employeeName, 0) + 1);
+            } else if (entryStatus.equals("-")) {
+                Integer count = employees.get(employeeName);
+                if (count == null || count == 0) {
+                    overtimeCount += 1;
+                } else {
+                    employees.put(employeeName, count - 1);
+                }
+            }
+        }
+
+        for (Integer value : employees.values()) {
+            if (value > 0) {
+                overtimeCount += value;
+            }
+        }
+
+        writer.write(String.valueOf(overtimeCount));
+        writer.flush();
+    }
+}
