9b98e7c6a5c04a1124239d39d95ca19bfd0383f1
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-05-02 22:02:28 +0900

    [Silver IV] Title: 야근, Time: 340 ms, Memory: 37500 KB -BaekjoonHub

--- 백준/Silver/14402. 야근/야근.java
+++ 백준/Silver/14402. 야근/야근.java
@@ -3,37 +3,52 @@ import java.util.*;
 
 public class Main {
 
+    private static final Map<String, Integer> EMPLOYEES_ENTRY_INFO = new HashMap<>();
+
+    private static int overtimeCount = 0;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-        Map<String, Integer> employees = new HashMap<>();
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+        var writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int entryCount = Integer.parseInt(reader.readLine());
-        int overtimeCount = 0;
+        var entryCount = Integer.parseInt(reader.readLine());
         for (int i = 0; i < entryCount; i++) {
-            String[] entryInfo = reader.readLine().split(" ");
-            String employeeName = entryInfo[0];
-            String entryStatus = entryInfo[1];
+            var tokenizer = new StringTokenizer(reader.readLine());
+            var employeeName = tokenizer.nextToken();
+            var entryStatus = tokenizer.nextToken();
 
-            if (entryStatus.equals("+")) {
-                employees.put(employeeName, employees.getOrDefault(employeeName, 0) + 1);
-            } else {
-                Integer count = employees.get(employeeName);
-                if (count == null || count == 0) {
-                    overtimeCount += 1;
-                } else {
-                    employees.put(employeeName, count - 1);
-                }
+            if (entryStatus.equals("-")) {
+                checkEntry(employeeName);
+                continue;
             }
+
+            recordEntry(employeeName);
         }
 
-        for (Integer value : employees.values()) {
+        calculateOvertime();
+
+        writer.write(String.valueOf(overtimeCount));
+        writer.flush();
+    }
+
+    private static void recordEntry(String employeeName) {
+        EMPLOYEES_ENTRY_INFO.put(employeeName, EMPLOYEES_ENTRY_INFO.getOrDefault(employeeName, 0) + 1);
+    }
+
+    private static void checkEntry(String employeeName) {
+        var entryCount = EMPLOYEES_ENTRY_INFO.get(employeeName);
+        if (entryCount == null || entryCount == 0) {
+            overtimeCount += 1;
+        } else {
+            EMPLOYEES_ENTRY_INFO.put(employeeName, entryCount - 1);
+        }
+    }
+
+    private static void calculateOvertime() {
+        for (var value : EMPLOYEES_ENTRY_INFO.values()) {
             if (value > 0) {
                 overtimeCount += value;
             }
         }
-
-        writer.write(String.valueOf(overtimeCount));
-        writer.flush();
     }
 }
