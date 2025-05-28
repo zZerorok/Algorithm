734e19b728142edd9c61ca21441ca2576f130009
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-05-28 21:33:18 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.53 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -3,12 +3,9 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
         Arrays.sort(numbers);
-        if (numbers[0] >= 0) {
-            return numbers[numbers.length - 2] * numbers[numbers.length - 1];
-        }
-
-        int minusMaxResult = numbers[0] * numbers[1];
-        int maxResult = numbers[numbers.length - 2] * numbers[numbers.length - 1];
-        return Math.max(minusMaxResult, maxResult);
+        int length = numbers.length;
+        int smallest = numbers[0] * numbers[1];
+        int largest = numbers[length - 2] * numbers[length - 1];
+        return Math.max(smallest, largest);
     }
 }
