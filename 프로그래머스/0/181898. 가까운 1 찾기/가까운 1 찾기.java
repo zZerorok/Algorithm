b7df8d1e0c3c42b102c7a5c981c7479362823513
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-05-31 22:36:11 +0900

    [level 0] Title: 가까운 1 찾기, Time: 2.69 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,10 +1,14 @@
+import java.util.stream.IntStream;
+
 class Solution {
+    
+    private final static int TARGET_NUMBER = 1;
+    private final static int NOT_FOUND = -1;
+    
     public int solution(int[] arr, int idx) {
-        for (int i = idx; i < arr.length; i++) {
-            if (arr[i] == 1) {
-                return i;
-            }
-        }
-        return -1;
+        return IntStream.range(idx, arr.length)
+                .filter(i -> arr[i] == TARGET_NUMBER)
+                .findFirst()
+                .orElse(NOT_FOUND);
     }
 }
