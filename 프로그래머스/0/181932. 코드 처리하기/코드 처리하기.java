4179ab21e6521de23d09c403f14a1903f7ac5dfa
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-06-15 15:12:55 +0900

    [level 0] Title: 코드 처리하기, Time: 65.61 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,31 +1,54 @@
 class Solution {
+    
+    public static final String MODE_CHANGE = "1";
+    private static final int EVEN = 0;
+
+    private static int MODE;
+    
     public String solution(String code) {
         StringBuilder result = new StringBuilder();
         String[] codes = code.split("");
-        String mode = "0";
         for (int i = 0; i < code.length(); i++) {
-
-            if (codes[i].equals("1")) {
-                if (mode.equals("0")) {
-                    mode = "1";
-                } else {
-                    mode = "0";
-                }
-            } else {
-                if (mode.equals("0") && (i % 2 == 0)) {
-                    result.append(codes[i]);
-                }
-
-                if (mode.equals("1") && (i % 2 != 0)) {
-                    result.append(codes[i]);
-                }
+            if (codes[i].equals(MODE_CHANGE)) {
+                changeMode();
+                continue;
             }
+            
+            operation(result, codes[i], i);
         }
 
-        if (result.toString().isEmpty()) {
-            return "EMPTY";
-        } else {
-            return result.toString();
+        if (isEmpty(result)) {
+            result.append("EMPTY");
         }
+
+        return result.toString();
+    }
+
+    private  void validateMode(String code) {
+        if (code.equals(MODE_CHANGE)) {
+            changeMode();
+        }
+    }
+
+    private void changeMode() {
+        MODE = 1 - MODE;
+    }
+
+    private void operation(StringBuilder result, String code, int index) {
+        if (isEven(index) || isOdd(index)) {
+            result.append(code);
+        }
+    }
+
+    private boolean isEven(int index) {
+        return MODE == EVEN && index % 2 == 0;
+    }
+
+    private boolean isOdd(int index) {
+        return MODE != EVEN && index % 2 != 0;
+    }
+
+    private boolean isEmpty(StringBuilder result) {
+        return result.toString().isEmpty();
     }
 }
