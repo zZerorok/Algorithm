7c14781f05c80cc10733da5c13962b4218056ea4
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-06-20 18:43:30 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.92 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -2,15 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
+        int[] result = Arrays.copyOf(arr, arr.length);
         for (int[] query : queries) {
             int start = query[0];
             int end = query[1];
 
             for (int i = start; i <= end; i++) {
-                arr[i] += 1;
+                result[i] += 1;
             }
         }
 
-        return arr;
+        return result;
     }
 }
