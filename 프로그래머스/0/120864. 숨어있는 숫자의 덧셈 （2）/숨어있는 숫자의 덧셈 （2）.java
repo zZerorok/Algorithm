6f7acd826322e726c2776d3409826fed5c9bfaf3
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-06-26 12:00:59 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.34 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -1,16 +1,27 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 class Solution {
     public int solution(String my_string) {
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(my_string);
-        
-        int sum = 0;
-        while (matcher.find()) {
-            sum += Integer.parseInt(matcher.group());
+        String replaced = alphabetToSpace(my_string);
+        if (replaced.isBlank()) {
+            return 0;
         }
-        
+
+        String[] splitSpace = removeSpace(replaced);
+        return getSum(splitSpace);
+    }
+
+    private String alphabetToSpace(String input) {
+        return input.replaceAll("[a-zA-Z]", " ");
+    }
+
+    private String[] removeSpace(String input) {
+        return input.trim().split(" +");
+    }
+
+    private static int getSum(String[] inputs) {
+        int sum = 0;
+        for (String input : inputs) {
+            sum += Integer.parseInt(input);
+        }
         return sum;
     }
 }
