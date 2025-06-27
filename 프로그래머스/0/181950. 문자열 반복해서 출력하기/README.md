1990fbcc7cd6b4be467a7157150aa0a56890f337
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-06-28 00:50:08 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 158.19 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/README.md
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 반복해서 출력하기 - 181950 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181950) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181950?language=java) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 173.78 ms
+메모리: 69.5 MB, 시간: 158.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 28일 00:45:53
+2025년 06월 28일 00:50:06
 
 ### 문제 설명
 
--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -1,13 +1,19 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String[] inputs = scanner.nextLine().split(" ");
-        String str = inputs[0];
-        int count = Integer.parseInt(inputs[1]);
-        String repeatStr = str.repeat(count);
-        System.out.println(repeatStr);
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            String[] inputTokens = reader.readLine().split(" ");
+            String value = inputTokens[0];
+            int count = Integer.parseInt(inputTokens[1]);
+
+            writer.write(value.repeat(count));
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
-                           
+}                    
