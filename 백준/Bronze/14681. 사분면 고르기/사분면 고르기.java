a38be5b9a8d60b04c9b35e2403e853b6704720aa
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-07-04 18:28:51 +0900

    [Bronze V] Title: 사분면 고르기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = Integer.parseInt(reader.readLine());
+        int y = Integer.parseInt(reader.readLine());
+
+        writer.write(String.valueOf(getQuadrant(x, y)));
+        writer.flush();
+    }
+
+    private static int getQuadrant(int x, int y) {
+        if (x > 0 && y > 0) {
+            return 1;
+        } else if (x < 0 && y > 0) {
+            return 2;
+        } else if (x < 0 && y < 0) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
