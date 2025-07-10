31dffb55f2675f6cd7141e10c20c181b9059920a
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-07-10 21:42:09 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,13 +1,11 @@
 class Solution {
 
-    private static final String TARGET = "7";
-
     public int solution(int[] array) {
         int count = 0;
         for (int number : array) {
-            String[] split = String.valueOf(number).split("");
-            for (String current : split) {
-                if (current.equals(TARGET)) {
+            String value = Integer.toString(number);
+            for (char ch : value.toCharArray()) {
+                if (ch == '7') {
                     count += 1;
                 }
             }
