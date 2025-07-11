a5a14a3fde5a592c1ef3a507260f3d0799a84cc8
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-07-11 18:54:54 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.17 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -2,24 +2,13 @@ import java.util.Map;
 
 class Solution {
 
-        static private final Map<String, String> DICTIONARY = Map.of(
-                "zero", "0",
-                "one", "1",
-                "two", "2",
-                "three", "3",
-                "four", "4",
-                "five", "5",
-                "six", "6",
-                "seven", "7",
-                "eight", "8",
-                "nine", "9"
-        );
-
     public long solution(String numbers) {
-        for (Map.Entry<String, String> entry : DICTIONARY.entrySet()) {
-            numbers = numbers.replace(entry.getKey(), entry.getValue());
-        }
+        String [] dictionary = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
+        for (int i = 0; i < dictionary.length; i++) {
+            numbers = numbers.replace(dictionary[i], String.valueOf(i));
+        }
+        
         return Long.parseLong(numbers);
     }
 }
