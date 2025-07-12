2e82cf491c97953e8fc2b9d5e804cabaafa2fc70
youngrok <156323937+zZerorok@users.noreply.github.com>
2025-07-12 22:47:46 +0900

    [Bronze II] Title: 콘테스트, Time: 112 ms, Memory: 15912 KB -BaekjoonHub

--- 백준/Bronze/5576. 콘테스트/콘테스트.java
+++ 백준/Bronze/5576. 콘테스트/콘테스트.java
@@ -3,33 +3,32 @@ import java.util.*;
 
 public class Main {
 
+    public static final int SCORE_COUNT = 10;
+    public static final int TOP_SCORE_COUNT = 3;
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<Integer> firstScores = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            firstScores.add(Integer.parseInt(reader.readLine()));
-        }
-        firstScores.sort(Comparator.reverseOrder());
-
-        int firstResult = 0;
-        for (int i = 0; i < 3; i++) {
-            firstResult += firstScores.get(i);
-        }
-
-        List<Integer> secondScores = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            secondScores.add(Integer.parseInt(reader.readLine()));
-        }
-        secondScores.sort(Comparator.reverseOrder());
-
-        int secondResult = 0;
-        for (int i = 0; i < 3; i++) {
-            secondResult += secondScores.get(i);
-        }
-
-        writer.write(firstResult + " " + secondResult);
+        int firstTotalScore = getTotalScore(reader);
+        int secondTotalScore = getTotalScore(reader);
+        
+        writer.write(firstTotalScore + " " + secondTotalScore);
         writer.flush();
+    }
+
+    private static int getTotalScore(BufferedReader reader) throws IOException {
+        List<Integer> scores = new ArrayList<>();
+        for (int i = 0; i < SCORE_COUNT; i++) {
+            scores.add(Integer.parseInt(reader.readLine()));
+        }
+
+        scores.sort(Comparator.reverseOrder());
+
+        int sum = 0;
+        for (int i = 0; i < TOP_SCORE_COUNT; i++) {
+            sum += scores.get(i);
+        }
+        return sum;
     }
 }
